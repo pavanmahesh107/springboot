aec9fd5f6724f1256c5f34d97fde8a3955baa40e
pavanmahesh1518 <pavanmahesh107@gmail.com>
2025-02-19 10:19:45 +0530

    Dynamic Intilization using 1st approach

--- src/main/java/dev/learningspringboot/Controller/DynamicInitialization/User.java
+++ src/main/java/dev/learningspringboot/Controller/DynamicInitialization/User.java
@@ -3,27 +3,31 @@ package dev.learningspringboot.Controller.DynamicInitialization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/")
 public class User {
-
-
     @Qualifier("onlineorderobject")
     @Autowired
-    Order order;
+    Order onlineOrderobj;
+
+    @Qualifier("offlineorderobject")
+    @Autowired
+    Order offlineOrderobj;
 
     public User(){
         System.out.println("User orders initialized");
     }
 
     @GetMapping(path = "/createorder")
-    public ResponseEntity<String> createOrder(){
-        order.createOrder();
+    public ResponseEntity<String> createOrder(@RequestParam boolean isOnlineOrder){
+
+        if(isOnlineOrder){
+            onlineOrderobj.createOrder();
+        } else {
+            offlineOrderobj.createOrder();
+        }
 
         return ResponseEntity.ok("");
     }
