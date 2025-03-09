37118c35d68458098069bc3f3e8863b7e0fdaabe
pavanmahesh1518 <pavanmahesh107@gmail.com>
2025-03-09 12:13:16 +0530

    Async Annotation Retun type with Completable Future

--- src/main/java/dev/learningspringboot/Controller/Async/UserServices.java
+++ src/main/java/dev/learningspringboot/Controller/Async/UserServices.java
@@ -4,13 +4,15 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 @Component
 public class UserServices {
 
     @Async
-    public Future<String> performTaskAsync(){
-        return new AsyncResult<>("Async Task Result");
+    public CompletableFuture<String> performTaskAsync(){  // We can use return type of @Async annotation with void,Future<T>, CompletableFuture<T>
+        // return new AsyncResult<>("Async Task Result from Future");
+        return CompletableFuture.completedFuture("Async Task result from Completable Future");
     }
 }
