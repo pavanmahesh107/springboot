00d05fe2c9f31890ddc79d55eea020c5172fff3f
pavanmahesh1518 <pavanmahesh107@gmail.com>
2025-03-11 17:31:18 +0530

    Monitoring Method Performance & Getting annotation

--- src/main/java/dev/learningspringboot/Controller/CustomInterceptor/CustomAnnotation/LoggingAspect.java
+++ src/main/java/dev/learningspringboot/Controller/CustomInterceptor/CustomAnnotation/LoggingAspect.java
@@ -3,7 +3,10 @@ package dev.learningspringboot.Controller.CustomInterceptor.CustomAnnotation;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -11,12 +14,16 @@ public class LoggingAspect {
 
     @Around("@annotation(dev.learningspringboot.Controller.CustomInterceptor.CustomAnnotation.LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable{
+        System.out.println("Do something before actual method");
+        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
+        if(method.isAnnotationPresent(LogExecutionTime.class)){
+            LogExecutionTime logExecutionTime = method.getAnnotation(LogExecutionTime.class);
+            System.out.println("Name from annotation " + logExecutionTime.name());
+        }
         long start = System.currentTimeMillis();
-
         Object result = joinPoint.proceed();
-
+        System.out.println("After the actual method");
         long end = System.currentTimeMillis();
-
         System.out.println(joinPoint.getSignature() + "executed in " + (end - start) + "ms");
         return result;
     }
--- src/main/java/dev/learningspringboot/Controller/CustomInterceptor/CustomAnnotation/UserService.java
+++ src/main/java/dev/learningspringboot/Controller/CustomInterceptor/CustomAnnotation/UserService.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    @LogExecutionTime
+    @LogExecutionTime(name = "user")
     public String getUser() throws InterruptedException{
         Thread.sleep(1000);
+        System.out.println("Get the user details");
         return "User Data";
-
     }
 }
